abd66d08dd96e2910fd1f1c6ac33851a3bc5827d
Uma <umas70204@gmail.com>
2024-10-07 12:25:42 +0530

    Program to print even and odd numbers

---
+++ index.js
@@ -0,0 +1,21 @@
+// to find the even and dd numbers 
+const numbers = [
+    3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,
+  ];
+  let flag =0;
+  for( let i = 0 ; i < numbers.length ; i++){
+    if(numbers[i] % 2 == 0){
+        console.log(numbers[i] , "even numbers");
+    }
+    else{
+        console.log(numbers[i] , "odd numbers" );
+    }
+  }
+
+//   if(flag > 0){
+//     console.log(numbers[i] , "even numbers");
+//   }
+//   else{
+//     console.log(numbers[i] , "odd numbers" );
+//   }
+  
